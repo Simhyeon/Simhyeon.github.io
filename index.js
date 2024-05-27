0c10167ea0c755e8025d0a9ba36ca5b17c47147d
simoncreek <simoncreek@tutanota.com>
2024-05-27 18:25:38 +0900

    TOggle js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+function toggle_study_style(buttonObject) {
+	let current_status = buttonObject.dataset.status;
+	let elems = document.getElementsByTagName("span");
+
+	/* Loop elems and set style*/
+	switch (current_status) {
+		case 'normal':
+			Array.from(elems).forEach(e => { e.classList.add('hidden') });
+			buttonObject.dataset.status = 'hidden';
+			break;
+		case 'hidden':
+			Array.from(elems).forEach(e => { 
+				e.classList.add('highlight');
+				e.classList.remove('hidden');
+				}
+			);
+			buttonObject.dataset.status = 'highlight';
+			break;
+		case 'highlight':
+			Array.from(elems).forEach(e => { e.classList.remove('highlight') });
+			buttonObject.dataset.status = 'normal';
+			break;
+		default:
+			break;
+			
+	}
+}
+
 function toggle_dark_mode(input) {
     if (input === "true") {
         localStorage.setItem("dark-mode", true); 
